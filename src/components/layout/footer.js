782f4282e4b9557b4a0a670329dba9167053a377
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2022-12-01 14:38:12 +0000

    little modification

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="layout-container py-4 md:py-8">
             <div className="flex flex-col md:flex-row items-center justify-between">
                 <Logo />
-                <p className="mt-2 matter-regular text-sm leading-6 text-b3 text-center md:text-left">Copyright © 2021 Dinerio technologies, Inc. <br/>
+                <p className="mt-2 matter-regular text-sm leading-6 text-b3 text-center md:text-left">Copyright © 2021 Zitere. <br/>
                     All rights reserved.
                 </p>
             </div>
